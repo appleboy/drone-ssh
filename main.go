34301a49d9fd01f8e83d8319aab3c032c4c68f58
Thomas Boerger <thomas@webhippie.de>
2016-02-21 11:06:41 +0100

    Make the builds reproducable based on commit sha

--- main.go
+++ main.go
@@ -14,11 +14,11 @@ import (
 )
 
 var (
-	buildDate string
+	buildCommit string
 )
 
 func main() {
-	fmt.Printf("Drone SSH Plugin built at %s\n", buildDate)
+	fmt.Printf("Drone SSH Plugin built from %s\n", buildCommit)
 
 	workspace := drone.Workspace{}
 	vargs := Params{}
